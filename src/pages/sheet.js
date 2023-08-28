250eb8b6ce885cec3b333bb16452095a18a47351
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-28 17:14:59 +0200

    addingintroandindex

---
+++ src/pages/sheet.js
@@ -0,0 +1,10 @@
+import MousePathTracker from "../../components/MouseTracker/MouseTracker";
+
+export default function Sheet() {
+  return (
+    <main className="main-container">
+      <div className="background-container">TEXT TEST</div>
+      <MousePathTracker />
+    </main>
+  );
+}
