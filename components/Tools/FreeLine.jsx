dc8e8d8f99d427960e169c7f66be04bd03a831fe
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-30 13:36:41 +0200

    downloadaspngdone

---
+++ components/Tools/FreeLine.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function FreeLine({ canvasRef }) {
+  const contextRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      canvas.width = 420 * 2;
+      canvas.height = 594 * 2;
+
+      canvas.style.width = "420px";
+      canvas.style.height = "594px";
+
+      const context = canvas.getContext("2d");
+      context.scale(1, 1);
+      context.lineCap = "round";
+      context.strokeStyle = "black";
+      context.lineWidth = 5;
+      contextRef.current = context;
+    }
+  }, [canvasRef]);
+
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = getMousePosition(
+      canvasRef.current,
+      nativeEvent
+    );
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = getMousePosition(
+      canvasRef.current,
+      nativeEvent
+    );
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
+  const getMousePosition = (canvas, event) => {
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+    const offsetX = (event.clientX - rect.left) * scaleX;
+    const offsetY = (event.clientY - rect.top) * scaleY;
+    return { offsetX, offsetY };
+  };
+
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  return (
+    <canvas
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+      style={{
+        maxWidth: "420px",
+        maxHeight: "594px",
+        background: "white",
+      }}></canvas>
+  );
+}
--- src/pages/tool.js
+++ src/pages/tool.js
@@ -1,5 +1,40 @@
-import Tool from "../../components/Tool/Tool";
+import FreeLine from "../../components/Tools/FreeLine";
+import { useRef } from "react";
 
-export default function drawPage() {
-  return <Tool />;
+export default function tool() {
+  const canvasRef = useRef(null);
+
+  const handleSaveClick = () => {
+    // const canvas = canvasRef.current;
+    // const image = canvas.toDataURL();
+    // const link = document.createElement("a");
+    // link.href = image;
+    // link.download = "canvas_image.jpg";
+    // link.click();
+    // console.log("Save button clicked", image);
+  };
+
+  const handleDownloadClick = () => {
+    const canvas = canvasRef.current;
+    const image = canvas.toDataURL();
+
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "canvas_image.jpg";
+    link.click();
+
+    console.log("Save button clicked", image);
+  };
+
+  return (
+    <div className="toolContainer">
+      <FreeLine canvasRef={canvasRef} />
+      <button className="saveButton" onClick={handleSaveClick}>
+        Save
+      </button>
+      <button className="downloadButton" onClick={handleDownloadClick}>
+        Download
+      </button>
+    </div>
+  );
 }
