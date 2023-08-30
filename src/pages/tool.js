0a959946836c80e139bb9673bd7f9392aad969b4
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-30 13:52:59 +0200

    jpgdownloaddone

--- src/pages/tool.js
+++ src/pages/tool.js
@@ -16,14 +16,29 @@ export default function tool() {
 
   const handleDownloadClick = () => {
     const canvas = canvasRef.current;
-    const image = canvas.toDataURL();
+    const newCanvas = document.createElement("canvas");
+    newCanvas.width = canvas.width;
+    newCanvas.height = canvas.height;
 
+    const newContext = newCanvas.getContext("2d");
+
+    // Fill the new canvas with white background
+    newContext.fillStyle = "white";
+    newContext.fillRect(0, 0, newCanvas.width, newCanvas.height);
+
+    // Draw the existing canvas content onto the new canvas
+    newContext.drawImage(canvas, 0, 0);
+
+    // Create a data URL for the new canvas (JPEG format)
+    const image = newCanvas.toDataURL("image/jpeg");
+
+    // Create a download link for the image
     const link = document.createElement("a");
     link.href = image;
     link.download = "canvas_image.jpg";
     link.click();
 
-    console.log("Save button clicked", image);
+    console.log("Download button clicked", image);
   };
 
   return (
