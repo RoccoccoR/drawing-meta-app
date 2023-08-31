471c1d854059d9c55968387a284c596c8f6bf7e6
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-31 15:13:34 +0200

    readbackendconnected

--- db/models/DrawModel.js
+++ db/models/DrawModel.js
@@ -1,25 +1,12 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const drawSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  img: {
-    type: Buffer, // Binary Data type for storing image data
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId, // Assuming you have a User schema
-    ref: "User", // Replace with your User model name
-    required: true,
-  },
-  publish: {
-    type: Boolean,
-    default: false,
-  },
+const { Schema } = mongoose;
+
+const drawSchema = new Schema({
+  title: { type: String, required: true },
+  user: { type: String, required: true },
 });
 
-const DrawModel = mongoose.model("Draw", drawSchema);
+const Draw = mongoose.models.Draw || mongoose.model("Draw", drawSchema);
 
-module.exports = DrawModel;
+export default Draw;
