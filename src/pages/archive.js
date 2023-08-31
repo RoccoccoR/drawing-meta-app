f2cffe30462532f16b2c4d022042a82fa7a8742b
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-31 15:58:45 +0200

    readinarchievedone

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -4,7 +4,16 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Archive() {
   const { data, error } = useSWR("/api/draws", fetcher);
-  console.log(data);
+  console.log("This is data:", data);
 
-  return <>Archive Page</>;
+  return (
+    <>
+      Archive Page
+      <div>
+        {data.map((drawing) => {
+          return <h1>{drawing._id}</h1>;
+        })}
+      </div>
+    </>
+  );
 }
