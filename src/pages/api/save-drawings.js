ac128ac198dff5ce4f93779163a0367ce8ece418
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-09-04 10:19:17 +0200

    verceling2

--- src/pages/api/save-drawings.js
+++ src/pages/api/save-drawings.js
@@ -1,37 +1,37 @@
-import dbConnect from "../../../db/connect";
-import Draw from "../../../db/models/DrawModel";
+// import dbConnect from "../../../db/connect";
+// import Draw from "../../../db/models/DrawModel";
 
-export default async function handler(request, response) {
-  await dbConnect();
-  if (request.method === "GET") {
-    try {
-      const drawings = await Draw.find();
-      return response.status(200).json(drawings);
-    } catch (error) {
-      console.log(error);
-      response.status(405).json({ message: "Method not allowed" });
-    }
-  } else if (request.method === "POST") {
-    try {
-      const { imageData, title, userId } = request.body;
+// export default async function handler(request, response) {
+//   await dbConnect();
+//   if (request.method === "GET") {
+//     try {
+//       const drawings = await Draw.find();
+//       return response.status(200).json(drawings);
+//     } catch (error) {
+//       console.log(error);
+//       response.status(405).json({ message: "Method not allowed" });
+//     }
+//   } else if (request.method === "POST") {
+//     try {
+//       const { imageData, title, userId } = request.body;
 
-      const newDrawing = new Draw({
-        imageData,
-        title,
-        userId,
-      });
+//       const newDrawing = new Draw({
+//         imageData,
+//         title,
+//         userId,
+//       });
 
-      console.log("Received POST request to save a new drawing");
-      console.log("Image Data:", imageData);
-      console.log("Title:", title);
-      console.log("User ID:", userId);
+//       console.log("Received POST request to save a new drawing");
+//       console.log("Image Data:", imageData);
+//       console.log("Title:", title);
+//       console.log("User ID:", userId);
 
-      await newDrawing.save();
+//       await newDrawing.save();
 
-      response.status(201).json({ status: "New drawing added" });
-    } catch (error) {
-      console.log(error);
-      response.status(400).json({ error: error.message });
-    }
-  }
-}
+//       response.status(201).json({ status: "New drawing added" });
+//     } catch (error) {
+//       console.log(error);
+//       response.status(400).json({ error: error.message });
+//     }
+//   }
+// }
