dbf784ea7de0bd6509ac9bb92474765fa7083d1e
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-09-10 16:44:45 +0200

    navbarhoverdone

--- components/IntroText/IntroText.js
+++ components/IntroText/IntroText.js
@@ -3,8 +3,8 @@ export default function IntroText() {
     <h1 className="introText">
       This is a placeholder text that explain briefly what this app does. If you
       start moving your mouse you can start explore a free line feature. When
-      you are done with your exploration just click on start to move to a new
-      empty screen where you can find more tools. Enjoy :)
+      you are done with your exploration just click on Sign in to move to a new
+      empty screen where you can start drawing. Enjoy :)
     </h1>
   );
 }
