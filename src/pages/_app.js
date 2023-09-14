cf47b5962849a990f5bf67f82a6c974c5c38e82a
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-09-14 12:30:08 +0200

    lll

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,17 +8,24 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const router = useRouter(); // Initialize the router
 
   useEffect(() => {
-    // Lock the screen orientation to portrait
-    async function lockScreenOrientation() {
-      try {
-        await screen.orientation.lock("portrait");
-        console.log("Screen orientation locked to portrait.");
-      } catch (error) {
-        console.error("Failed to lock screen orientation:", error);
+    // Check if screen orientation locking is supported by the browser
+    if ("orientation" in screen) {
+      // Lock the screen orientation to portrait
+      async function lockScreenOrientation() {
+        try {
+          await screen.orientation.lock("portrait");
+          console.log("Screen orientation locked to portrait.");
+        } catch (error) {
+          console.error("Failed to lock screen orientation:", error);
+        }
       }
-    }
 
-    lockScreenOrientation(); // Call the function to lock the orientation when the component mounts
+      lockScreenOrientation(); // Call the function to lock the orientation when the component mounts
+    } else {
+      console.log(
+        "Screen orientation locking is not supported on this device/browser."
+      );
+    }
   }, []); // The empty dependency array ensures this effect runs only once when the component mounts
 
   return (
