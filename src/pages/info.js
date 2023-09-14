8f36672a753ba6dc76e4c6786812c24be7ca4096
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-09-14 17:12:49 +0200

    a

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,25 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { isMobile } from "react-device-detect";
 
 export default function Info() {
-  const [isLandscape, setIsLandscape] = useState(false);
-
-  useEffect(() => {
-    function handleOrientationChange() {
-      setIsLandscape(window.innerWidth > window.innerHeight);
-    }
-
-    window.addEventListener("orientationchange", handleOrientationChange);
-    handleOrientationChange(); // Initial check
-
-    return () => {
-      window.removeEventListener("orientationchange", handleOrientationChange);
-    };
-  }, []);
+  const isLandscape = window.matchMedia("(orientation: landscape)").matches;
 
   return (
     <div>
-      {isMobile && isLandscape && <h1>Please rotate your device :)</h1>}
+      {isMobile && isLandscape && <h1>Please rotate your device</h1>}
       {!isMobile && <h1>Test</h1>}
     </div>
   );
