de687d3efd890fc7a17eed85d5ae5d751db2fed3
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-09-15 12:28:54 +0200

    fontactually

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,9 @@ import "@/styles/globals.css";
 import Layout from "../../components/Layout/Layout";
 import { SessionProvider } from "next-auth/react";
 import { useRouter } from "next/router"; // Import the useRouter hook
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const router = useRouter(); // Initialize the router
@@ -11,11 +14,27 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       <SessionProvider session={session}>
         {router.pathname === "/" ? (
           // Don't render Layout on the root page
-          <Component {...pageProps} />
+          <>
+            <style jsx global>{`
+              html {
+                font-family: ${inter.style.fontFamily};
+              }
+            `}</style>
+
+            <Component {...pageProps} />
+          </>
         ) : (
           // Render Layout on all other pages
           <Layout>
-            <Component {...pageProps} />
+            <>
+              <style jsx global>{`
+                html {
+                  font-family: ${inter.style.fontFamily};
+                }
+              `}</style>
+
+              <Component {...pageProps} />
+            </>
           </Layout>
         )}
       </SessionProvider>
