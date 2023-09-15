c7e53493d0cd04682a197e359716e73a5491af9c
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-09-16 01:29:42 +0200

    w

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,21 @@ import Layout from "../../components/Layout/Layout";
 import { SessionProvider } from "next-auth/react";
 import { useRouter } from "next/router"; // Import the useRouter hook
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const router = useRouter(); // Initialize the router
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+
+    // Check the current route and add a class to the body element for specific pages
+    if (router.pathname === "/tool") {
+      body.classList.add("backgroundWhite");
+    }
+  }, [router.pathname]);
 
   return (
     <>
